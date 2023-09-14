cee13b71623457e14ed66bfe8342a2b6095b5eef
Jaimin2002 <jaiminparikh12@gmail.com>
2023-09-15 00:03:56 +0530

    renamed main project file

--- 100_day/DAY16/project/main.py
+++ 100_day/DAY16/project/main.py
@@ -1,3 +1,4 @@
+'''On this Day i have made same project as yesterday but using OOp concepts!! Observe how LOC reduces'''
 from menu import Menu, MenuItem
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
